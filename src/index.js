6cc50a60491f31e5ffbfc70f63ca91ac2a7b309f
Olena <kryvtsovaolena@gmail.com>
2023-02-17 20:34:22 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { App } from './components/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-weather-app">
-      <App />
-    </BrowserRouter>
+    {/* <BrowserRouter basename="/react-weather-app"> */}
+    <App />
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
